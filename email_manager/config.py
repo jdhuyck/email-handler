bd0dbb04a8c9cd644a2ae93df29d749f71d4a887
Jody Huyck-Jones <68274088+jdhuyck@users.noreply.github.com>
2025-07-18 22:06:22 +0100

    Bump version to 0.2.0

---
+++ email_manager/config.py
@@ -0,0 +1,28 @@
+import dataclasses
+
+from app_confetti import util
+
+
+def optional_str(v):
+    return v if v.lower() != "none" else None
+
+@dataclasses.dataclass
+class Config:
+    gmail_token: str = util.env("GMAIL_TOKEN: None", optional_str)
+    gmail_refresh_token: str = util.env("GMAIL_REFESH_TOKEN:None", optional_str)
+    gmail_client_id: str = util.env("GMAIL_CLIENT_ID:None", optional_str)
+    gmail_client_secret: str = util.env("GMAIL_CLIENT_SECRET:None", optional_str)
+
+    @property
+    def gmail_token_json(self):
+        if self.gmail_token is not None:
+            return {
+                "token": self.gmail_token,
+                "refresh_token": self.gmail_refresh_token,
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "client_id": self.gmail_client_id,
+                "client_secret": self.gmail_client_secret,
+                "scopes": ["https://mail.google.com/"],
+            }
+        else:
+            return {}
---
+++ email_manager/gmail_client.py
@@ -0,0 +1,91 @@
+from math import trunc
+from pathlib import Path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from email_manager.config import Config
+import json
+import datetime
+from typing import Optional
+
+
+class TokenAuthentication:
+    def __init__(self, scopes: list = ["https://mail.google.com/"]):
+        self.scopes = scopes
+
+    def gmail_authenticate(self, token: json):
+        return build("gmail", "v1", credentials=Credentials.from_authorized_user_info(token))
+
+    def create_token(self, client_config: dict, token_path: str):
+        # Used to create gmail_token.json - only call in offline scripts
+        creds = None
+        token_file = Path(token_path)
+
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_config(client_config, self.scopes)
+            creds = flow.run_local_server(port=0)
+
+        token_data = {
+            "token": creds.token,
+            "refresh_token": creds.refresh_token,
+            "token_uri": creds.token_uri,
+            "client_id": creds.client_id,
+            "client_secret": creds.client_secret,
+            "scopes": creds.scopes,
+        }
+
+        with token_file.open("w") as token:
+            json.dump(token_data, token)
+
+
+class EmailFetcher:
+    def __init__(self, token: json):
+        self.service = TokenAuthentication.gmail_authenticate(self, token)
+
+    def _parse_message(self, message):
+        msg = self.service.users().messages().get(
+            userId="me",
+            id=message["id"],
+            format="full"
+        ).execute
+
+        return msg
+
+    def search_messages(self, service, query):
+        result = service.users().messages().list(userId="me", q=query).execute()
+        messages = []
+        if "messages" in result:
+            messages.extend(result["messages"])
+        while "nextPageToken" in result:
+            page_token = result["nextPageToken"]
+            result = service.users().messages().list(userId="me", q=query, pageToken=page_token).execute()
+            if "messages" in result:
+                messages.extend(result["messages"])
+        return messages
+
+    def fetch_emails(
+        self,
+        since: datetime.datetime,
+        before: Optional[datetime.datetime] = None
+    ) -> list:
+        query = f"after:{trunc(since.timestamp())}"
+        if before is not None:
+            query = f"{query} before:{trunc(before.timestamp())}"
+
+        messages = self.search_messages(self.service, query)
+        return [self._parse_message(msg) for msg in messages]
+
+
+if __name__ == "__main__":
+    client = TokenAuthentication(["https://mail.google.com/"])
+    credentials = json.load(open("credentials/gmail_credentials.json", "rb"))
+    token = client.create_token(client_config=credentials, token_path="credentials/gmail_credentials.json")
+
+    # settings = Config()
+    # since = datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(1)
+
+    # fetcher = EmailFetcher(token=token)
+    # emails = fetcher.fetch_emails(since)
